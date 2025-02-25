060b068909469bcb3dfffe35c12f7f8b38aaf62c
Samuel-miller <samuel.m.soares10@aluno.senai.br>
2025-02-25 16:48:30 -0300

    Lista de Exercícios Python

---
+++ Lista de Exercícios - LOPAL/Lista_de_exercicios_01/exercicio01.py
@@ -0,0 +1,7 @@
+#EXERCÍCIO 01 - SOMA DE DOIS NÚMEROS
+#PEDE OS NÚMEROS AO USUÁRIOS
+numero01 = float(input("Insira o primeiro número: "))
+numero02 = float(input("Insira o segundo número: "))
+
+#IMPRIME O RESULTADO
+print(f"A soma dos números {numero01:.1f} e {numero02:.1f} é {(numero01+numero02):.1f}")
