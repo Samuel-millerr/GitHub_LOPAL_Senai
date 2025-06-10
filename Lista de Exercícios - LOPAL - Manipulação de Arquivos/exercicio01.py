22dddf1a29384f16bf0fdfefaa2ca23e878d56bb
Samuel-millerr <samuel.m.soares10@aluno.senai.br>
2025-06-10 15:47:59 -0300

    Atividade Python - Manipulação de Arquivos

--- Lista de Exercícios - LOPAL - Manipulação de Arquivos/exercicio01.py
+++ Lista de Exercícios - LOPAL - Manipulação de Arquivos/exercicio01.py
@@ -1,4 +1,20 @@
-import openpyxl
+import pandas as pd
+#
+# historico_pedidos = [
+# {'ID': 1, 'Nome': 'João', 'Endereço': 'Rua das Flores, 123', 'Produto': 'Camiseta', 'Quantidade': 2, 'Preço': 50, 'Data': '01/01/2023'},
+# {'ID': 2, 'Nome': 'Mariana', 'Endereço': 'Avenida Central, 456', 'Produto': 'Tênis', 'Quantidade': 1, 'Preço': 120, 'Data': '02/01/2023'},
+# {'ID': 3, 'Nome': 'Carlos', 'Endereço': 'Praça da Estação, 789', 'Produto': 'Mochila', 'Quantidade': 1, 'Preço': 80, 'Data': '03/01/2023'},
+# {'ID': 4, 'Nome': 'Fernanda', 'Endereço': 'Alameda dos Anjos, 101', 'Produto': 'Relógio', 'Quantidade': 1, 'Preço': 150, 'Data': '04/01/2023'}
+# ]
+#
+# workbook = openpyxl.Workbook()
+# sheet = workbook.active
+# sheet.append(["ID", "Nome", "Endereço", "Produto", "Quantidade", "Preço", "Data"])
+#
+# for pedido in historico_pedidos:
+#     sheet.append([pedido["ID"], pedido["Nome"], pedido["Endereço"], pedido["Produto"], pedido["Quantidade"], pedido["Preço"], pedido["Data"]])
+#
+# workbook.save("exercicio01Dados.xlsx")
 
 historico_pedidos = [
 {'ID': 1, 'Nome': 'João', 'Endereço': 'Rua das Flores, 123', 'Produto': 'Camiseta', 'Quantidade': 2, 'Preço': 50, 'Data': '01/01/2023'},
@@ -7,11 +23,5 @@ historico_pedidos = [
 {'ID': 4, 'Nome': 'Fernanda', 'Endereço': 'Alameda dos Anjos, 101', 'Produto': 'Relógio', 'Quantidade': 1, 'Preço': 150, 'Data': '04/01/2023'}
 ]
 
-workbook = openpyxl.Workbook()
-sheet = workbook.active
-sheet.append(["ID", "Nome", "Endereço", "Produto", "Quantidade", "Preço", "Data"])
-
-for pedido in historico_pedidos:
-    sheet.append([pedido["ID"], pedido["Nome"], pedido["Endereço"], pedido["Produto"], pedido["Quantidade"], pedido["Preço"], pedido["Data"]])
-
-workbook.save("exercicio01Dados.xlsx")
+df = pd.DataFrame(historico_pedidos)
+df.to_excel("dados.xlsx", index = False)
